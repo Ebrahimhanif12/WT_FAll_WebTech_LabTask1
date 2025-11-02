323335f01205b661b5c513d930f466f71a2823db
Ebrahimhanif12 <ebrahimhanif810@gmail.com>
2025-11-02 12:09:45 +0600

    first commit

---
+++ LabTask.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  
+<style>
+    input{
+        width: 100%;
+        padding: 2px;
+        height: 20px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+</style>
+
+</head>
+<body>
+    <center>
+    <h1 style="text-align: center;">Clinick Patient Registratin</h1>
+
+    <div style="ackground-color: #f9f9f9; width: 550px; text-align: left; padding: 10px; border-radius: 8px; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+        <p>First Name:</p>
+        <input type="text">
+        <p>Age:</p>
+        <input  type="text">
+        <p>Phone Number:</p>
+        <input type="text">
+        <p>Email Address:</p>
+        <input type="text">
+        <p>Insurance Provider:</p>
+        <select style="width:100%">
+            <option value="1">Select Provider</option>
+            <option value="2">Provider 1</option>
+            <option value="3">Provider 2</option>
+            <option value="4">Provider 3</option>
+            <option value="5">Provider 4</option>
+        </select>
+        <p>Insurance Policy Number:</p>
+        <input  type="text">
+
+        <p style="font-weight: bold; text-align: center;">Additional Information:</p>
+
+        <p>Username</p>
+        <input type="text">
+        <p>Password</p>
+        <input  type="password"> <br><br>
+        <button style="background-color: blue; width: 100%; color: white; border: none; padding: 10px 15px; cursor: pointer;" type="submit">Submit</button>
+
+    </div>
+</center>
+</body>
+
+</html>
+
